f7d2bcb8fc0cc0259ced5c8e254385743bf5663b
Fawwaz Firdaus <muhammadfawwaz463@gmail.com>
2022-05-22 10:52:49 +0700

    Menambahkan page homeV1, carousel ambil dari library

---
+++ assets/script/categoryCarouse.js
@@ -0,0 +1,11 @@
+var splide = new Splide('.splide', {
+    type: 'loop',
+    perPage: 5,
+    width: '1149px',
+    height: '220px',
+    autoplay: true,
+    gap: 28,
+    pagination: false
+});
+
+splide.mount();
---
+++ assets/script/trendCarousel.js
@@ -0,0 +1,33 @@
+var owl = $('.owl-carousel');
+
+owl.owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    touchDrag: true,
+    mouseDrag: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    items: 2,
+    stagePadding: 50,
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 3
+        },
+        1000:{
+            items: 4
+        }
+    }
+})
+
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
